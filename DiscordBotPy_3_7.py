e42db1b07a4bb5ca22a5c1d9611327c2eb9a7c0b
William Beck <echarger1@gmail.com>
2019-07-06 17:21:14 -0400

    Added Beta Kick and Change nickname command

---
+++ DiscordBotPy_3_7.py
@@ -0,0 +1,76 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='$')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def add(ctx, a: int, b: int):
+    await ctx.send(a+b)
+
+@bot.command()
+async def multiply(ctx, a: int, b: int):
+    await ctx.send(a*b)
+
+@bot.command()
+async def greet(ctx):
+    await ctx.send(":smiley: :wave: Hello, there!")
+
+@bot.command()
+async def cat(ctx):
+    await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
+
+@bot.command()
+async def info(ctx):
+    embed = discord.Embed(title="nice bot", description="Nicest bot there is ever.", color=0xeee657)
+
+    # give info about you here
+    embed.add_field(name="Author", value="<YOUR-USERNAME>")
+
+    # Shows the number of servers the bot is member of.
+    embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
+
+    # give users a link to invite thsi bot to their server
+    embed.add_field(name="Invite", value="[Invite link](<insert your OAuth invitation link here>)")
+
+    await ctx.send(embed=embed)
+
+bot.remove_command('help')
+
+@bot.command()
+async def help(ctx):
+    embed = discord.Embed(title="nice bot", description="A Very Nice bot. List of commands are:", color=0xeee657)
+
+    embed.add_field(name="$add X Y", value="Gives the addition of **X** and **Y**", inline=False)
+    embed.add_field(name="$multiply X Y", value="Gives the multiplication of **X** and **Y**", inline=False)
+    embed.add_field(name="$greet", value="Gives a nice greet message", inline=False)
+    embed.add_field(name="$cat", value="Gives a cute cat gif to lighten up the mood.", inline=False)
+    embed.add_field(name="$info", value="Gives a little info about the bot", inline=False)
+    embed.add_field(name="$help", value="Gives this message", inline=False)
+
+    await ctx.send(embed=embed)
+
+@bot.command(pass_context=True)
+async def kick(ctx, user_name: discord.User):
+    await bot.kick(user_name)
+
+@client.event
+async def on_ready():
+    print("ToLate Special Operations Reporting For Duty")
+    await client.change_presence(game=discord.Game(name="By Slay > $enlist", type=3))
+    print("For more information: Please contact Slay on twitter @OverflowEIP")
+
+@client.command(pass_context=True)
+async def enlist(ctx, *, nickname):
+    await client.change_nickname(ctx.message.author, nickname)
+    role = get(ctx.message.server.roles, name='ARMA_ROLE') # Replace ARMA_ROLE as appropriate
+    if role: # If get could find the role
+        await client.add_role(ctx.message.author, role)
+
+bot.run('Token')
